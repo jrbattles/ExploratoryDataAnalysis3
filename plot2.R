5efd567e003362301a67d37f03fc5a56fb76da87
Jason Battles <jason.r.battles@gmail.com>
2016-04-15 23:33:50 -0500

    added plot1.R and plot1.png

--- plot2.R
+++ plot2.R
@@ -5,12 +5,12 @@
 # Jason R. Battles
 # jason.r.battles@gmail.com
 
-##  Obtain full dataset.  Remove NA values
+## Getting full dataset.  Remove NA values
 data_full <- read.csv("../data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
-## Subsetting the data and remove full dataset.
+## Subsetting the data  and remove full dataset.
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
 
@@ -18,10 +18,8 @@ rm(data_full)
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
-## Plot 1
-hist(data$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-
-## Saving to file
-dev.copy(png, file="plot1.png", height=480, width=480)
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
